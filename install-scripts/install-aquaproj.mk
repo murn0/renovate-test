e299cec7050e5c86e5892fc3057e8d125b2df812
murn0 <47878863+murn0@users.noreply.github.com>
2023-07-19 13:53:45 +0900

    test aqua-installer checksum

---
+++ install-scripts/install-aquaproj.mk
@@ -0,0 +1,12 @@
+include $(PWD)/bin/colors.mk	
+
+.PHONY: $(shell cat $(MAKEFILE_LIST) | awk -F':' '/^[a-z0-9_-]+:/ {print $$1}')	
+
+all: install-aqua	
+
+install-aqua: ## Install aqua (https://github.com/aquaproj/aqua)	
+	@curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.1.1/aqua-installer	
+	@echo "c2af02bdd15da6794f9c98db40332c804224930212f553a805425441f8331665  aqua-installer" | sha256sum -c	
+	@chmod +x aqua-installer	
+	@./aqua-installer -v v2.9.0	
+	@$(RM) ./aqua-installer	
