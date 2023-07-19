5caaa12643942f61ef946c8a40955c76a91f5096
murn0 <47878863+murn0@users.noreply.github.com>
2023-07-19 14:35:37 +0900

    update.

--- install-scripts/install-aquaproj.mk
+++ install-scripts/install-aquaproj.mk
@@ -5,8 +5,8 @@ include $(PWD)/bin/colors.mk
 all: install-aqua	
 
 install-aqua: ## Install aqua (https://github.com/aquaproj/aqua)	
-	@curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.1.1/aqua-installer	
-	@echo "c2af02bdd15da6794f9c98db40332c804224930212f553a805425441f8331665  aqua-installer" | sha256sum -c	
-	@chmod +x aqua-installer	
-	@./aqua-installer -v v2.9.0	
-	@$(RM) ./aqua-installer	
+	@curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.1.1/aqua-installer
+	@echo "c2af02bdd15da6794f9c98db40332c804224930212f553a805425441f8331665  aqua-installer" | sha256sum -c
+	@chmod +x aqua-installer
+	@./aqua-installer -v v2.9.1
+	@$(RM) ./aqua-installer
